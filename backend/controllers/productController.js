a538aa637ea9b8ae5566de2aa030c5865c7a01c2
AM-SAPP <8923499556nikhil@gmail.com>
2021-11-09 00:18:44 +0530

    added update product and delete product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require('../models/productModel');
 
 
-// Create Product 
+// Create Product - Admin
 
 exports.createProduct = async (req,res,next)=>{
     const product = await Product.create(req.body);
@@ -13,6 +13,59 @@ exports.createProduct = async (req,res,next)=>{
 }
 
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message : "ROute working correctly"})
+// Get all products
+exports.getAllProducts = async (req,res)=>{
+    const products = await Product.find();
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+
+// Update product --Admin
+exports.updateProduct = async (req,res,next)=>{
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+
+// Delete Product --Admin
+
+exports.deleteProduct = async (req,res)=>{
+
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Product deleted successfully"
+    })
 }
