e78225df70547faf685262abf0f2cf98c0156855
AM-SAPP <8923499556nikhil@gmail.com>
2021-11-09 12:15:04 +0530

    handled uncaught errors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
+// Config
+
 require("dotenv").config({path: "backend/config/config.env"});
+
+
 const app = require('./app');
 const connectdb = require('./config/database');
 
+// Handling Uncaught Rejection
 
+process.on("uncaughtException",(err)=>{
+    console.log(`Error : ${err.message}`);
+    console.log(`Shutting Down the server due to uncaught errors`);
+    process.exit(1);
+})
+
+
+
+// Connecting to db
 connectdb();
 const server = app.listen(process.env.PORT,()=>{
     console.log(`listening to http://localhost:${process.env.PORT}`);
 });
 
+
+// Unhandled Promise Rejection
 
 process.on("unhandledRejection",(err)=>{
     console.log(`Error : ${err.message}`);
