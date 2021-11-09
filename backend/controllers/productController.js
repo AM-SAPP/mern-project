b805fec9e60dc2e54e61e219da41f2de9995a114
AM-SAPP <8923499556nikhil@gmail.com>
2021-11-09 13:41:14 +0530

    added filter feature

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,7 +18,7 @@ exports.createProduct = catchAsyncError(async (req,res,next)=>{
 // Get all products
 exports.getAllProducts = catchAsyncError(async (req,res)=>{
 
-    const apiFeature = new ApiFeatures(Product.find(),req.query).search();
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter();
     const products = await apiFeature.query;
 
     res.status(200).json({
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -18,6 +18,35 @@ class ApiFeatures {
         return this;
     }
 
+
+    filter(){
+
+        // Do not use this because it use reference of this.queryStr ,so if we change queryReference then this.queryStr will also change.
+
+        // const queryReference = this.queryStr;
+
+        // this codes not change this.queryStr as it creates a copy;
+
+
+        const queryCopy = {...this.queryStr};
+
+        // Remove some field for category;
+        const removeFields = ["keyword","page","limit"];
+
+        removeFields.forEach(key=> delete queryCopy[key]);
+
+        // Filter for price and Rating
+
+        let queryStr = JSON.stringify(queryCopy).replace(/\b(gt|lt|gte|lte)\b/g,key=>`$${key}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+
+        this.query = this.query.find(queryCopy);
+
+        return this;
+
+    }
+
 }
 
 
