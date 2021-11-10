f6c99172c925e3bf96e4d9c3d687bb29449437b5
AM-SAPP <8923499556nikhil@gmail.com>
2021-11-10 13:50:17 +0530

    new error handler added

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,6 +12,26 @@ module.exports = (err,req,res,next)=>{
         err = new ErrorHandler(message,400);
     }
 
+    // Mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message,400);
+    }
+
+    // Wrong json web token terror
+    if(err.name === "JsonwebTokenError"){
+        const message = `json web token is invalid try again`;
+        err = new ErrorHandler(message,400);
+    }
+
+
+    // Json token expire
+    if(err.name === "TokenExpiredError"){
+        const message = `Json web token is Expired , try again`;
+        err = new ErrorHandler(message,400);
+    }
+
+
 
     res.status(err.statusCode).json({
         success: false,
